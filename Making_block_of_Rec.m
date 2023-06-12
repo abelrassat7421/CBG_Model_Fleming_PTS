2e41139bf366f272c05e18e339bbdcc8206c282e
Abel Armand Rassat <abel.rassat@epfl.ch>
2023-06-12 09:29:00 +0200

    modifying Matlab script

--- Making_block_of_Rec.m
+++ Making_block_of_Rec.m
@@ -1,21 +1,26 @@
 % Set up parameters
-amplitudes = 0:0.5:4;
+amplitudes = 3;
 phases = 0:30:330;
 num_amplitudes = length(amplitudes);
 num_phases = length(phases);
 signal_size = 72001;
 
+seeds = [1825,410,4507,4013,3658,2287,1680,8936,1425,9675,6913,521,489,1536,3583,3812,8280,9864,435,9196,3258,8929,6874,3612,7360,9655,4558,107,2616,6925,5575,4553,2548,3528,5515,1675,1520,6225,1585,5882,5636,9892,4334,712,7528,8786,2046,6202,1292,9045];
+num_seeds = length(seeds);
+
 % Create empty block
-STN_LFP_Recordings = zeros([num_phases, num_amplitudes, signal_size]);
+STN_LFP_Recordings_3mA = zeros([num_phases, num_seeds, signal_size]);
+i = 600;
 
 % Loop over recordings for different amplitudes and phases, loading data into block
-for jj = 1:num_amplitudes
-    for kk = 1:num_phases
-        directory_name = 'Full_simulations';
-        folder_name = sprintf('Rec_%.1fmA_%ddeg', amplitudes(jj), phases(kk));
-        file_name = sprintf('STN_LFP_%.1fmA-%.1fdeg.mat', amplitudes(jj), phases(kk));  
+for kk = 1:num_phases
+    for jj = 1:num_seeds
+        i = i+1;
+        directory_name = 'PTS_1_3mA';
+        folder_name = sprintf('PTS-%d', i);
+        file_name = sprintf('STN_LFP_3.0mA-%.1fdeg-%dseed.mat', phases(kk), seeds(jj));  
         full_path = fullfile(directory_name, folder_name, file_name);
         data = load(full_path);
-        STN_LFP_Recordings(kk, jj, :) = data.block.segments{1, 1}.analogsignals{1, 1}.signal;  
+        STN_LFP_Recordings_3mA(kk, jj, :) = data.block.segments{1, 1}.analogsignals{1, 1}.signal;  
     end
 end
